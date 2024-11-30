e5a5d208b161dc2ae2be23273e40178042626975
ElNao <naopubg28@gmail.com>
2024-11-30 10:51:15 -0600

    se agrego el modulo de parametros

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ParametersModule } from './parameters/parameters.module';
 
 @Module({
-  imports: [],
+  imports: [ParametersModule],
   controllers: [AppController],
   providers: [AppService],
 })
